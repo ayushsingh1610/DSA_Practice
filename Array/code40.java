7532186aec7c1efdc4dd9a1a900eaf44d5f5d9e6
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-01-29 13:39:04 +0530

    Update code40

--- Array/code40.java
+++ Array/code40.java
@@ -1,5 +1,7 @@
+
 // Write a Java program to replace every element with the next greatest element (from the right side) in a given array of integers. There is no element next to the last element, therefore replace it with -1. 
 import java.util.*;
+
 public class code40 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,21 +10,17 @@ public class code40 {
         int a[] = new int[l];
         System.out.print("Enter the elements in array = ");
         for (int i = 0; i < a.length; i++) {
-            a[i] =sc.nextInt();
+            a[i] = sc.nextInt();
         }
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if(a[i]>a[j])
-                {
-                    int temp = a[i];
+            for (int j = i; j < a.length; j++) {
+                if (a[i] < a[j]) {
                     a[i] = a[j];
-                    a[j] = temp;
                     break;
-                }
-                else
-                a[i] = -1;
+                } else
+                    a[i] = -1;
             }
-            
+
         }
         for (int i : a) {
             System.out.print(i + " ");
