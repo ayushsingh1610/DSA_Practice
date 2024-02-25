5699266d3f6c17542602d49b13b8afbfa0f723b6
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-25 21:52:06 +0530

    Update code53

--- Array/code53.java
+++ Array/code53.java
@@ -43,7 +43,7 @@ public class code53
     // Optimal method
     public static List<List<Integer>> four_Sum_2(int a[],int target)
     {
-        Set<List<Integer>> st = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(a);
 
         for(int i=0;i<a.length;i++)
@@ -51,16 +51,30 @@ public class code53
             if(i>0 && a[i]==a[i-1])     continue;
             for(int j=i+1; j<a.length;j++)
             {
-                if(j!=i+1 && a[j]==a[j-1])      continue;
-            int k = j+1;
-            int l = a.length-1;
+                if(j>i+1 && a[j]==a[j-1])      continue;
+                int k = j+1;
+                int l = a.length-1;
 
             while(k<l)
             {
-                int sum = a[i] + a[j] + a[k] + a[l];
-                if(sum>target)
+                long sum = a[i];
+                sum+=a[j];
+                sum+=a[k];
+                sum+=a[l];
+                if(sum==target)
                 {
-                    l--;
+                    List<Integer> temp = new ArrayList<>();
+                    temp = Arrays.asList(a[i], a[j], a[k], a[l]);
+                    // temp.add(a[i]);
+                    // temp.add(a[j]);
+                    // temp.add(a[k]);
+                    // temp.add(a[l]);
+                    ans.add(temp);
+                    k++; l--;
+
+
+                    while(k<l && a[k]==a[k-1])  k++;
+                    while(k<l && a[l]==a[l+1])  l--;
                 }
                 else if(sum<target)
                 {
@@ -68,17 +82,12 @@ public class code53
                 }
                 else
                 {
-                    List<Integer> temp = new ArrayList<>();
-                    temp = Arrays.asList(a[i], a[j], a[k], a[l]);
-                    st.add(temp);
-                    k++; l--;
-                    while(k<l && a[k]==a[k-1])  k++;
-                    while(k<l && a[l]==a[l+1])  l--;
+                    l--;
                 }
             }
         }
     }
-        List<List<Integer>> ans = new ArrayList<>(st);
+        
         return ans;
     }
 
