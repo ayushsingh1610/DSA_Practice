f2c100d7ac407e404c1eaf881b908d47d8c3869b
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-27 23:36:45 +0530

    Add code54.java

---
+++ Array/code54.java
@@ -0,0 +1,26 @@
+// Number of Subarray
+import java.util.*;
+public class code54
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a[] = {4, 2, 2, 6, 4};
+        System.out.println("Number of subarray = " + number_of_subarray(a, 6));
+    }
+    // Brute force
+    public static int number_of_subarray(int a[],int k)
+    {
+        int count =0;
+
+        for (int i = 0; i < a.length; i++) {
+            int sum =0;
+            for (int j = i; j < a.length; j++) {
+                sum+=a[j];
+
+                if(sum==k)      count++;
+            }
+        }
+        return count;
+    }
+}
